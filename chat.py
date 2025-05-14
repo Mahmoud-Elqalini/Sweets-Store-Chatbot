131641ad85e1341d8750792731701ad15499a560
Mahmoud-Elqalinicls <mahmoudeq02@gmail.com>
2025-05-14 19:47:58 +0300

    new updated

--- chat.py
+++ chat.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify, render_template
 import numpy as np
 import torch
 import torch.nn as nn
+from decimal import Decimal
 import re
+import json
 import pyodbc
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from main_functions import (load_json_file, NeuralNet, bag_of_words, preprocess, evaluate_math_expression,
@@ -16,7 +18,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 # Linear layer to transform embedding from 768 to 1152
-embedding_transform = nn.Linear(768, 1176).to('cuda' if torch.cuda.is_available() else 'cpu')
+embedding_transform = nn.Linear(768, 1787).to('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Connect to SQL Server
 try:
@@ -31,6 +33,48 @@ try:
 except Exception as error:
     print(f"Error: Failed to connect to database: {str(error)}")
     exit()
+
+
+def extract_product_name(user_input):
+    try:
+        # Execute an SQL query to fetch all product names.
+        cursor.execute("SELECT product_name FROM products")  # Replace 'products' with the correct table name.
+        products = [row.product_name for row in cursor.fetchall()]
+
+        # Input Cleaning: Convert to lowercase and remove special characters
+        user_input_clean = re.sub(r'[^\w\s]', '', user_input.lower().strip())  # Remove symbols like (?)
+
+        # Remove common words
+        common_words = {"what", "is", "the", "of", "on", "a", "there", "discount", "price"}
+        input_words = [word for word in user_input_clean.split() if word not in common_words]
+        cleaned_input = " ".join(input_words)  # Merging important words
+
+        # Step one: Full match inspection
+        for product in products:
+            if cleaned_input == product.lower():
+                return product  # Return the fully matching product and stop
+
+        # Step two: Partial match inspection
+        matching_products = []
+        for product in products:
+            product_lower = product.lower()
+            if any(word in product_lower for word in input_words):
+                matching_products.append(product)
+
+        # Remove duplicates
+        matching_products = list(set(matching_products))
+
+        # Returning matching products
+        if matching_products:
+            if len(matching_products) == 1:
+                return matching_products[0]  # Return the only product
+            else:
+                return matching_products  # Return the list of products
+        else:
+            return "The product name was not found in the question."
+
+    except Exception as e:
+        return f"An error occurred while connecting to the database: {e}"
 
 
 # Database schema retrieval
@@ -110,6 +154,117 @@ except FileNotFoundError:
     exit()
 
 
+def process_product_query(user_input, tag, language):
+    try:
+        # Calling the function to extract the product name
+        product_name = extract_product_name(user_input)
+        print(product_name)
+
+        # Check if the output is a string (one product) or a list (more than one product).
+        if isinstance(product_name, str):
+            # One product stat
+            sql_query = "EXEC SearchProduct @ProductName = ?"
+            cursor.execute(sql_query, product_name)
+            result = cursor.fetchone()
+
+            # Data extraction
+            stock = result[3]
+            discount = float(result[4]) if isinstance(result[4], Decimal) else result[4]
+            price = float(result[2]) if isinstance(result[2], Decimal) else result[2]
+            price_after_discount = price * (1 - discount / 100)
+
+            # Specify the response based on the tag
+            if tag == "product_availability":
+                print(stock)
+                if stock > 0:
+                    response = f"نعم، عندنا {product_name}!" if language == 'ar' \
+                        else f"Yes, we have {product_name} in stock!"
+                else:
+                    response = f"آسف، {product_name} مش متوفر في المخزون." if language == 'ar' \
+                        else f"Sorry, {product_name} is out of stock."
+            elif tag == "offers_and_discounts":
+                print(discount)
+                response = f"فيه عرض بـ {discount}% دلوقتي!" if language == 'ar' \
+                    else f"There’s a {discount}% discount available now!"
+            elif tag == "product_price":
+                print(price)
+                response = f"سعر {product_name} قبل الخصم هو {price} $، بعد الخصم هو {price_after_discount:.2f} $." \
+                    if language == 'ar' \
+                    else f"The price of {product_name.capitalize()} before discount is ${price}, " \
+                         f"and after discount is ${price_after_discount:.2f}."
+            else:
+                response = result[2]
+
+        elif isinstance(product_name, list):
+            # The case of more than one product
+            products_info = []
+            for product in product_name:
+                sql_query = "EXEC SearchProduct @ProductName = ?"
+                cursor.execute(sql_query, product)
+                result = cursor.fetchone()
+
+                if result:
+                    stock = result[3]
+                    discount = float(result[4]) if isinstance(result[4], Decimal) else result[4]
+                    price = float(result[2]) if isinstance(result[2], Decimal) else result[2]
+                    price_after_discount = price * (1 - discount / 100)
+
+                    # Add product details to the list
+                    product_details = {
+                        "product_name": product,
+                        "stock": stock,
+                        "price": price,
+                        "discount": discount,
+                        "price_after_discount": round(price_after_discount, 2)
+                    }
+                    products_info.append(product_details)
+
+            # Return the list of products with details
+            if products_info:
+                if tag == "product_availability":
+                    response = f"\nوجدت أكثر من منتج: " if language == 'ar' \
+                        else f"Multiple products found:\n "
+                    response += "\n".join(
+                        [f"{p['product_name']}: {'متوفر' if p['stock'] > 0 else 'غير متوفر'}\n" if language == 'ar'
+                         else f"{p['product_name']}: {'In stock' if p['stock'] > 0 else 'Out of stock'}\n"
+                         for p in products_info]
+                    )
+                elif tag == "offers_and_discounts":
+                    response = f"العروض المتاحة للمنتجات':\n" if language == 'ar' \
+                        else f"Available offers:\n"
+                    response += "\n".join(
+                        [f"{p['product_name']}: خصم {p['discount']}%\n"
+                         if language == 'ar' else f"{p['product_name']}: {p['discount']}% discount\n"
+                         for p in products_info]
+                    )
+                elif tag == "product_price":
+                    response = f"أسعار المنتجات: \n" if language == 'ar' \
+                        else f"Prices for products:\n"
+                    response += "\n".join(
+                        [f"{p['product_name']}: قبل الخصم {p['price']} $، بعد الخصم {p['price_after_discount']} $\n"
+                         if language == 'ar'
+                         else f"{p['product_name']}: Before discount ${p['price']}, after discount $"
+                              f"{p['price_after_discount']}\n"
+                         for p in products_info]
+                    )
+                else:
+                    response = json.dumps(products_info)  # Return JSON if the tag is unknown
+            else:
+                response = "لم يتم العثور على أي منتجات." if language == 'ar' else "No products found."
+
+        else:
+            response = "لم يتم العثور على اسم المنتج في السؤال." if language == 'ar' \
+                else "Product name not found in the query."
+
+        print(f"Response: {response}")
+        return jsonify({"response": response})
+
+    except Exception as e:
+        response = f"خطأ في تنفيذ الاستعلام" if language == 'ar' else f"Error executing query"
+        print(f"Database query error: {str(e)}")
+        return jsonify({"response": response})
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -128,7 +283,6 @@ def chat():
 
     # Preprocess and predict
     if language == 'ar':
-        # Get AraBERT embedding as PyTorch tensor
         try:
             embedding = get_arabert_embedding(user_input)
             # Transform and reshape to match input_size
@@ -139,8 +293,8 @@ def chat():
                 raise ValueError(f"Embedding size {input_tensor.size(1)} does not match model input_size {input_size}")
         except Exception as e:
             print(f"Arabic processing error: {str(e)}")
-            response = f"آسف، فيه مشكلة في معالجة النص! الخطأ: {str(e)}" if language == 'ar' \
-                else f"Sorry, there was an issue processing the text! Error: {str(e)}"
+            response = f"آسف، فيه مشكلة في معالجة النص!" if language == 'ar' \
+                else f"Sorry, there was an issue processing the text!"
             return jsonify({"response": response})
     else:
         tokens = preprocess(user_input, language)
@@ -162,52 +316,10 @@ def chat():
         except Exception as e:
             response = "آسف، مش قادر أحل المعادلة دي!" if language == 'ar' else "Sorry, I can't solve that equation!"
             print(f"Math evaluation error: {str(e)}")
-        print(f"Response: {response}")
         return jsonify({"response": response})
 
-    # Handle database queries
-    if tag in ["product_availability", "offers_and_discounts", "product_price", "database_query"]:
-        try:
-            sql_query = generate_sql_with_dynamic_schema(user_request=user_input, language=language)
-            print(f"Generated SQL Query: {sql_query}")
-            if not is_valid_sql_query(sql_query):
-                response = "آسف، الاستعلام مش آمن للتنفيذ." if language == 'ar' \
-                    else "Sorry, the generated query is not safe to execute."
-                print("Unsafe SQL query detected")
-                return jsonify({"response": response})
-
-            cursor.execute(sql_query)
-            result = cursor.fetchall()
-            if result:
-                if tag == "product_availability":
-                    stock = result[0][0]
-                    product_name = user_input.split()[-2] if language == 'ar' else user_input.lower().split()[-2]
-                    if stock > 0:
-                        response = f"نعم، عندنا {product_name} في المخزون!" if language == 'ar' \
-                            else f"Yes, we have {product_name.capitalize()} in stock!"
-                    else:
-                        response = f"آسف، {product_name} مش متوفر في المخزون." if language == 'ar' \
-                            else f"Sorry, {product_name.capitalize()} is out of stock."
-                elif tag == "offers_and_discounts":
-                    discount = result[0][0]
-                    response = f"فيه عرض بـ {discount}% دلوقتي!" if language == 'ar' \
-                        else f"There’s a {discount}% discount available now!"
-                elif tag == "product_price":
-                    price = result[0][0]
-                    product_name = user_input.split()[-2] if language == 'ar' else user_input.lower().split()[-2]
-                    response = f"سعر {product_name} هو {price} جنيه." if language == 'ar' \
-                        else f"The price of {product_name.capitalize()} is ${price}."
-                else:
-                    response = str(result)  # Generic response for other database queries
-            else:
-                response = "آسف، ما لقيتش نتايج للطلب ده." if language == 'ar' \
-                    else "Sorry, I couldn't find any results for your request."
-            print(f"Response: {response}")
-            return jsonify({"response": response})
-        except Exception as e:
-            response = f"خطأ في تنفيذ الاستعلام: {str(e)}" if language == 'ar' else f"Error executing query: {str(e)}"
-            print(f"Database query error: {str(e)}")
-            return jsonify({"response": response})
+    if tag in ["product_availability", "offers_and_discounts", "product_price"]:
+        return process_product_query(user_input, tag, language)
 
     # Handle general intents
     for intent in intents_to_use['intents']:
@@ -222,6 +334,5 @@ def chat():
     return jsonify({"response": response})
 
 
-if __name__ == ('__'
-                'main__'):
+if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
